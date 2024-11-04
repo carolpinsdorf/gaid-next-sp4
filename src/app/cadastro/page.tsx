d418dc85eb057379f381cd4f7786e91c574aeee7
carolpinsdorf <carolinapinsdorf@gmail.com>
2024-11-04 13:13:33 -0300

    fix cadastro

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -15,7 +15,7 @@ export default function Cadastro() {
         nomeCliente: '',
         dataNascimento: '',
         acesso: {
-            id: '',
+            id: 0,
             emailAcesso: '',
             username: '',
             senha: '',
